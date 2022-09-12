2dde1b33f07a6d966370c5c8bdf6448ac9c52857
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-13 00:33:13 +0300

    Category Routes

---
+++ routes/category.routes.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { verifyLogin } from "../Middleware/Verification.js";
+import { validate } from 'express-validation';
+import { createCategory, deleteCategory, getCategories, getCategoryById, updateCategory } from "../controllers/category.controllers.js";
+import categoryValidations from '../Validations/category.validations.js';
+
+const router = express.Router();
+
+// @route   GET /categories
+// @desc    Get all categories
+// @access  Public
+
+router.get('/categories', verifyLogin, getCategories);
+
+// @route   GET /category/categoryId
+// @desc    Get category
+// @access  Public
+
+router.get('/category/categoryId', verifyLogin, getCategoryById);
+
+// @route   POST /category
+// @desc    Create category
+// @access  Public
+
+router.post('/category', verifyLogin, validate(categoryValidations.categoryValidator), createCategory);
+
+// @route   PUT /category/:id
+// @desc    Update category
+// @access  Public
+
+router.put('/category/:categoryId', verifyLogin, updateCategory);
+
+// @route   DELETE /category/:categoryId
+// @desc    Delete category
+// @access  Public
+
+router.delete('/category/:categoryId', verifyLogin, deleteCategory);
+
+export default router;
