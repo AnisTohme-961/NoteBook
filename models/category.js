f4316c0c97bafbfca8d0660e811ba0210781173a
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-13 00:32:30 +0300

    Category Model

---
+++ models/category.js
@@ -0,0 +1,14 @@
+import mongoose, { Schema } from "mongoose";
+
+const categorySchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    writtenBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'    
+    }
+}, { timestamps: true })
+
+export default mongoose.model('Category', categorySchema);
