bb9633897f473fc15a804d42bea046ed3b33b1ae
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-13 00:33:00 +0300

    User Model

---
+++ models/user.js
@@ -0,0 +1,35 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, 'First Name is required.']
+    },
+    lastName: {
+        type: String, 
+        required: [true, 'Last Name is required.']
+    },
+    userName: {
+        type: String, 
+        required: [true, 'Username is required.']
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required.'],
+        unique: true
+    },
+    password: {
+        type: String, 
+        required: [true, 'Password is required.']
+    },
+    category: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Category'
+    }],
+    note: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Note'
+    }]
+}, { timestamps: true })
+
+export default mongoose.model('User', userSchema);
