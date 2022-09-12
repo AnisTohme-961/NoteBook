09f507a9f6c94aaa58856f94a2015256a829062f
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-13 00:34:24 +0300

    Category Validations

---
+++ Validations/category.validations.js
@@ -0,0 +1,13 @@
+import joi from "joi";
+import joi.objectId from "joi-objectid"(joi);
+
+const categoryValidations = {
+    categoryValidator: {
+        body: joi.object({
+            title: joi.string().required().min(5),
+            writtenBy: joi.objectId().required()
+        })
+    }
+}
+
+export default categoryValidations;
