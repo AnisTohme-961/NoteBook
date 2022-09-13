9d85896d5a719213650c0f41f8165dba564ba903
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-13 14:29:47 +0300

    Note Validations

--- Validations/note.validations.js
+++ Validations/note.validations.js
@@ -1,14 +1,16 @@
 import joi from "joi";
 // import joi.objectId from "joi-objectid"(joi);
+import JoiObjectId from "joi-objectid";
+const myJoiObjectId = JoiObjectId(joi);
 
 const noteValidations = {
     noteValidator: {
         body: joi.object({
            title: joi.string().required().min(7).alphanum(),
            description: joi.string().required().min(25).alphanum(),
-           writtenBy: joi.objectId().required(),
+           writtenBy: myJoiObjectId().required(),
            status: joi.string().valid('completed','pending').default('pending'),
-           categoryId: joi.objectId().required(),
+           categoryId: myJoiObjectId().required(),
            tags: joi.array().items(joi.string()).optional().default('null')
         })
     }
