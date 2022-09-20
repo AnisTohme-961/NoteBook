8b7994ca847355c8a35535af2b92b60243240725
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-20 22:52:08 +0300

    Auth Routes

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,7 @@
-import { login, signUp } from "../controllers/auth.js";
-<<<<<<< HEAD
-import Validator from "../Middleware/Validator.js";
-=======
-
-import Validator from "../Middleware/Validator.js";
 import express from "express";
-import userValidations from '../Validations/user.validations.js';
->>>>>>> bc11d7d15879641e1e2c5755de53c1407cf8b789
-import { validate } from "express-validation";
+import { login, signUp } from "../controllers/auth.js";
+import Validator from "../Middleware/Validator.js";
+
 
 const router = express.Router();
 
@@ -15,11 +9,7 @@ const router = express.Router();
 // @desc    User sign up
 // @access  Private
 
-<<<<<<< HEAD
 router.post('/signup', Validator('signup') , signUp);
-=======
-router.post('/signup', Validator('signup'), signUp);
->>>>>>> bc11d7d15879641e1e2c5755de53c1407cf8b789
 
 // @route   POST /login
 // @desc    User login
