13d7ee3be1a7db9e4806ca01913cadfbce0420e0
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-20 22:34:08 +0300

    Note Validator

---
+++ Validations/note.validator.js
@@ -0,0 +1,12 @@
+import Joi from "joi";
+import JoiObjectId from "joi-objectid";
+
+const myJoiObjectId = JoiObjectId(Joi);
+
+const noteSchema = Joi.object({
+    title: Joi.string().min(3).max(30).required(),
+    content: Joi.string().min(3).max(1000).required(),
+    writtenBy: myJoiObjectId().required()
+});
+
+export default noteSchema;
