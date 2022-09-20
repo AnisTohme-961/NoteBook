4f0436de1d969e8bbf88e25439212e54cfff8745
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-20 22:35:32 +0300

    Note Routes

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { verifyLogin } from '../Middleware/Verification.js';
 import { validate } from 'express-validation';
 import { createNote, deleteNote, getNoteById, getNotes, updateNote } from '../controllers/note.controllers.js';
-import noteValidations from '../Validations/note.validations.js';
+import Validator from '../Middleware/Validator.js';
 
 const router = express.Router();
 
@@ -22,7 +22,7 @@ router.get('/note/noteId', verifyLogin, getNoteById);
 // @desc    Create note
 // @access  Public
 
-router.post('/note', verifyLogin, validate(noteValidations.noteValidator), createNote);
+router.post('/note', verifyLogin, Validator('note'), createNote);
 
 // @route   PUT /note/:noteId
 // @desc    Update note
