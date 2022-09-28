ab7fa6b555b2795313dbda37785518c20f5c5342
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-28 18:42:33 +0300

    Note Routes

--- routes/note.routes.js
+++ routes/note.routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyLogin } from '../Middleware/Verification.js';
-import { createNote, deleteNote, getNoteById, getNotes, updateNote } from '../controllers/note.controllers.js';
+import { createNote, deleteNote, getFirstAndLastTag, getNoteById, getNotes, getNotesWithTags, updateNote } from '../controllers/note.controllers.js';
 import Validator from '../Middleware/Validator.js';
 
 const router = express.Router();
@@ -16,6 +16,18 @@ router.get('/', verifyLogin, getNotes);
 // @access  Private
 
 router.get('/:noteId', verifyLogin, getNoteById);
+
+// @route   Get /:noteId/tags
+// @desc    Get first and last tags of note
+// @access  Private
+
+router.get('/:noteId/fltags', verifyLogin, getFirstAndLastTag);
+
+// @route   Get /tag
+// @desc    Get notes that has tags and its size
+// @access  Private
+
+router.get('/:noteId/tags', verifyLogin, getNotesWithTags);
 
 // @route   POST /note
 // @desc    Create note
