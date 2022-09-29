a3db352fed73ec4ad49339cac72b9f553305328d
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:44:30 +0300

    App File

--- app.js
+++ app.js
@@ -1,44 +1,36 @@
-import ErrorHandler from "./Middleware/ErrorHandler.js";
-import authRoutes from "./routes/auth.routes.js";
-import categoryRoutes from "./routes/category.routes.js";
-import databaseConnection from "./config/dbconnect.js";
-import dotenv from "dotenv";
-import express from "express";
-import noteRoutes from "./routes/note.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import ErrorHandler from "./Middleware/ErrorHandler.js"
+import authRoutes from "./routes/auth.routes.js"
+import categoryRoutes from "./routes/category.routes.js"
+import databaseConnection from "./config/dbconnect.js"
+import dotenv from "dotenv"
+import express from "express"
+import noteRoutes from "./routes/note.routes.js"
+import userRoutes from "./routes/user.routes.js"
 
-dotenv.config();
+dotenv.config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-app.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "Welcome To The NoteBook API",
-        author: "Anis Tohme",
-        repository: "https://github.com/AnisTohme-961/NoteBook"
-    })
+app.get("/", (req, res, next) => {
+  res.status(200).json({
+    message: "Welcome To The NoteBook API",
+    author: "Anis Tohme",
+    repository: "https://github.com/AnisTohme-961/NoteBook",
+  })
 })
 
-app.use("/auth", authRoutes);
-app.use("/users", userRoutes);
-app.use("/notes", noteRoutes);
-app.use("/categories", categoryRoutes);
+app.use("/auth", authRoutes)
+app.use("/users", userRoutes)
+app.use("/notes", noteRoutes)
+app.use("/categories", categoryRoutes)
 
-app.use(ErrorHandler);
+app.use(ErrorHandler)
 
 app.listen(PORT, async () => {
-    await databaseConnection();
-    console.log(`Server is running on port ${PORT}`)
+  await databaseConnection()
+  console.log(`Server is running on port ${PORT}`)
 })
-
-
-
-
-
-
-
-
