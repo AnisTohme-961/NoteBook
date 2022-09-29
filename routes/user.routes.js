e8204b835cadd4ee44e32391f6ad2fb9faebae31
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:46:46 +0300

    User Routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,38 +1,44 @@
-import express from 'express';
-import { verifyLogin } from '../Middleware/Verification.js';
-import { changePassword, deleteUser, getUserById, getUsers, updateUser } from '../controllers/user.controllers.js';
+import express from "express"
+import { verifyLogin } from "../Middleware/Verification.js"
+import {
+  changePassword,
+  deleteUser,
+  getUserById,
+  getUsers,
+  updateUser,
+} from "../controllers/user.controllers.js"
 import Validator from "../Middleware/Validator.js"
 
-const router = express.Router();
+const router = express.Router()
 
 // @route   GET /users/
 // @desc    Get all users
 // @access  Private
 
-router.get('/', verifyLogin, getUsers);
+router.get("/", verifyLogin, getUsers)
 
 // @route   GET /user/id
 // @desc    Get user
 // @access  Private
 
-router.get('/:id', verifyLogin, getUserById);
+router.get("/:id", verifyLogin, getUserById)
 
 // @route   PUT /
 // @desc    Update user
 // @access  Private
 
-router.put('/', verifyLogin, Validator('user'), updateUser);
+router.put("/", verifyLogin, Validator("user"), updateUser)
 
 // @route   DELETE /users/
 // @desc    Delete user
 // @access  Private
 
-router.delete('/', verifyLogin, deleteUser);
+router.delete("/", verifyLogin, deleteUser)
 
 // @route   PATCH /users/
 // @desc    Change password
 // @access  Private
 
-router.patch('/', verifyLogin, Validator('password'), changePassword);
+router.patch("/", verifyLogin, Validator("password"), changePassword)
 
-export default router;
+export default router
