a370f3506c3672cf2a684260a0a2d4230a04d5a0
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:48:23 +0300

    Signup Validation

--- Validations/signup.validator.js
+++ Validations/signup.validator.js
@@ -1,13 +1,13 @@
-import Joi from "joi";
+import Joi from "joi"
 
-const passwordValidator = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$@^%&? "])[a-zA-Z0-9!#$@^%&?]{8,20}$/ 
+const passwordValidator = /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[a-zA-Z!#$@^%&? "])[a-zA-Z0-9!#$@^%&?]{8,20}$/
 
 const signupSchema = Joi.object({
-    firstName: Joi.string().min(3).max(30),
-    lastName: Joi.string().min(3).max(30),
-    username: Joi.string().required().min(3).max(30),
-    email: Joi.string().email().required(),
-    password: Joi.string().required().pattern(passwordValidator)
-});
+  firstName: Joi.string().min(3).max(30),
+  lastName: Joi.string().min(3).max(30),
+  username: Joi.string().required().min(3).max(30),
+  email: Joi.string().email().required(),
+  password: Joi.string().required().pattern(passwordValidator),
+})
 
-export default signupSchema;
+export default signupSchema
