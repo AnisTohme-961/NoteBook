72785f580c5fbcc5fbd82c4aada839caa7908707
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:46:28 +0300

    Category Routes

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,38 +1,44 @@
-import express from 'express';
-import { verifyLogin } from "../Middleware/Verification.js";
-import { createCategory, deleteCategory, getCategories, getCategoryById, updateCategory } from "../controllers/category.controllers.js";
-import Validator from '../Middleware/Validator.js';
+import express from "express"
+import { verifyLogin } from "../Middleware/Verification.js"
+import {
+  createCategory,
+  deleteCategory,
+  getCategories,
+  getCategoryById,
+  updateCategory,
+} from "../controllers/category.controllers.js"
+import Validator from "../Middleware/Validator.js"
 
-const router = express.Router();
+const router = express.Router()
 
 // @route   GET /categories
 // @desc    Get all categories
 // @access  Private
 
-router.get('/', verifyLogin, getCategories);
+router.get("/", verifyLogin, getCategories)
 
 // @route   GET /categories/:categoryId
 // @desc    Get category
 // @access  Private
 
-router.get('/:categoryId', verifyLogin, getCategoryById);
+router.get("/:categoryId", verifyLogin, getCategoryById)
 
 // @route   POST /categories/
 // @desc    Create category
 // @access  Private
 
-router.post('/', verifyLogin, Validator('category'), createCategory);
+router.post("/", verifyLogin, Validator("category"), createCategory)
 
 // @route   PUT /categories/:categoryid
 // @desc    Update category
 // @access  Private
 
-router.put('/:categoryId', verifyLogin, Validator('category'), updateCategory);
+router.put("/:categoryId", verifyLogin, Validator("category"), updateCategory)
 
 // @route   DELETE /category/:categoryId
 // @desc    Delete category
 // @access  Private
 
-router.delete('/:categoryId', verifyLogin, deleteCategory);
+router.delete("/:categoryId", verifyLogin, deleteCategory)
 
-export default router;
+export default router
