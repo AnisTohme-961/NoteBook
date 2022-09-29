41d8911c9275d08ded149508dec2dcacaaddb3e5
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:47:43 +0300

    Index Validation

--- Validations/index.js
+++ Validations/index.js
@@ -1,17 +1,17 @@
-import login from "./login.validator.js";
-import signup from "./signup.validator.js";
-import note from "./note.validator.js";
-import category from "./category.validator.js";
-import user from "./user.validator.js";
-import password from "./password.validator.js";
+import login from "./login.validator.js"
+import signup from "./signup.validator.js"
+import note from "./note.validator.js"
+import category from "./category.validator.js"
+import user from "./user.validator.js"
+import password from "./password.validator.js"
 
 const Validators = {
-    login,
-    signup,
-    note,
-    category,
-    user,
-    password
+  login,
+  signup,
+  note,
+  category,
+  user,
+  password,
 }
 
-export default Validators;
+export default Validators
