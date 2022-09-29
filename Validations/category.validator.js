d25da072c03830a308d82c22f0c51836afaf8027
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:47:33 +0300

    Category Validation

--- Validations/category.validator.js
+++ Validations/category.validator.js
@@ -1,11 +1,11 @@
-import Joi from "joi";
-import JoiObjectId from "joi-objectid";
+import Joi from "joi"
+import JoiObjectId from "joi-objectid"
 
-const myJoiObjectId = JoiObjectId(Joi);
+const myJoiObjectId = JoiObjectId(Joi)
 
 const categorySchema = Joi.object({
-    title: Joi.string().required().min(5),
-    description: Joi.string().min(3).max(100).required(),
+  title: Joi.string().required().min(5),
+  description: Joi.string().min(3).max(100).required(),
 })
 
-export default categorySchema;
+export default categorySchema
