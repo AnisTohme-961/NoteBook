67165745fd5c527a88f6726a35b5a4808a84a3f5
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:48:00 +0300

    Note Validation

--- Validations/note.validator.js
+++ Validations/note.validator.js
@@ -1,14 +1,14 @@
-import Joi from "joi";
-import JoiObjectId from "joi-objectid";
+import Joi from "joi"
+import JoiObjectId from "joi-objectid"
 
-const myJoiObjectId = JoiObjectId(Joi);
+const myJoiObjectId = JoiObjectId(Joi)
 
 const noteSchema = Joi.object({
-    title: Joi.string().min(3).max(30).required(),
-    content: Joi.string().min(3).max(1000).required(),
-    status: Joi.string().valid('pending', 'complete'),
-    tags: Joi.array().items(Joi.string()),
-    categoryId: myJoiObjectId('Category')
-});
+  title: Joi.string().min(3).max(30).required(),
+  content: Joi.string().min(3).max(1000).required(),
+  status: Joi.string().valid("pending", "complete"),
+  tags: Joi.array().items(Joi.string()),
+  categoryId: myJoiObjectId("Category"),
+})
 
-export default noteSchema;
+export default noteSchema
