74aca95fe75940697d278550071412745d3a8c49
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:45:49 +0300

    Note Model

--- models/note.js
+++ models/note.js
@@ -1,37 +1,39 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose"
 
-const noteSchema = mongoose.Schema({
+const noteSchema = mongoose.Schema(
+  {
     title: {
-        type: String, 
-        required: true,
-        unique: true
+      type: String,
+      required: true,
+      unique: true,
     },
     content: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     writtenBy: {
-        type: Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
     status: {
-        type: String, 
-        enum: ["complete", "pending"],
-        default: "pending"
+      type: String,
+      enum: ["complete", "pending"],
+      default: "pending",
     },
     categoryId: {
-        type: Schema.Types.ObjectId,
-        ref: 'Category',
-        required: true
+      type: Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
     },
     tags: {
-        type: [String],
-        required: false,
-        default: null,
-        lowercase: true
-    }
-    
-}, { timestamps: true })
+      type: [String],
+      required: false,
+      default: null,
+      lowercase: true,
+    },
+  },
+  { timestamps: true }
+)
 
-export default mongoose.model('Note', noteSchema);
+export default mongoose.model("Note", noteSchema)
