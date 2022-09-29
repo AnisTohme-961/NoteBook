5e8642bff1f92bd42dbd82d1db00c7e285152890
AnisTohme-961 <anistohme1@hotmail.com>
2022-09-29 18:45:40 +0300

    Category Model

--- models/category.js
+++ models/category.js
@@ -1,18 +1,21 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose"
 
-const categorySchema = mongoose.Schema({
+const categorySchema = mongoose.Schema(
+  {
     title: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     writtenBy: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'    
-    }
-}, { timestamps: true })
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+)
 
-export default mongoose.model('Category', categorySchema);
+export default mongoose.model("Category", categorySchema)
